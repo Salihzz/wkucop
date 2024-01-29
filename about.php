ed01482d9cbb5c42682f6cb523ff29ab688baa3c
Salihzz <157997594+Salihzz@users.noreply.github.com>
2024-01-30 00:59:59 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,74 @@
+ <?php 
+ ?>
+ 
+    <body id="class_div">
+		<?php
+		?>
+		
+		<link href="Chart.js" rel="stylesheet">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								
+                            <div class="block-content collapse in">
+							<h3 align="center">Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="images/3.png" class="img-circle img-responsive">
+										<hr>
+										<p>Name: Abdulrasool Said Abdulrasool Hamed</p>
+										<p>Address: Nyala City</p>
+										<p>Email: Abdulrasool99@gmail.com</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="images/1.jpg" class="img-circle">
+								<hr>
+																				<p>Name: Abdelslam Siddig omer</p>
+
+										<p>Address: Nyala City</p>
+										<p>Email: abdelslamsiddig@gmail.com</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="images/7.jpg" class="img-circle">
+								<hr>
+												<p>Name: elsadig elhady ahmed</p>
+										<p>Address: Alnahoud City</p>
+										<p>Email: elsadigelhady@gmail.com</p>
+										<p>Position: Project Manager</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="images/6.jpg" class="img-circle">
+								<hr>
+												<p>Name: Hammad Jibril</p>
+										<p>Address: Alnahoud City</p>
+										<p>Email:  Hammad Jibrilgmail.com</p>
+										<p>Position: Project Manager</p>
+								</center>
+								</div>
+								
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-center"></i> Back</a></div>
+								</div>
+								
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ home.php
@@ -0,0 +1,27 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>West Kurdofan Universty</h4>
+    <p> </p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Faculty Of Computer Science And Information Technology</h4> 
+<br>
+  <p>Faculty Of Computer Science And Information Technology Was Established In 2014.</p>
+ <br>
+<h4>The College Grants The Science In Following Discipline</h4> 
+  <ol>
+    <li>Computer Science.</li>  
+    <li>Information Technology</li> 
+    <li>Information System</li> 
+  </ol>
+
+
+</div>
+<div  class="col-lg-6">
+   
+    </ul>
+
+</div>
+ 
+
